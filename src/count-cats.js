dec18af60cda4764fc46c98f3e1085e989950564
vrNKg <veronika.gamalia@mail.ru>
2022-10-17 18:09:09 +0400

    ready tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-// function countCats(/* matrix */) {
-//   throw new NotImplementedError('Not implemented');
-//   // remove line with error and write your code here
-// }
+
 function countCats(backyard) {
   let count = 0,
   cat = '^^';
